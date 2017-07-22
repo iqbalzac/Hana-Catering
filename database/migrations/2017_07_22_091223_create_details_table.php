cc601305feaa4849aea764b7340d0bf5ba97c046
iqbal zaky <ibonkbolungs@gmail.com>
2017-07-22 17:13:48 +0700

    create database

---
+++ database/migrations/2017_07_22_091223_create_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->increments('id_menu');
+            $table->string('id_psn');
+            $table->float('jumlah');
+            $table->float('tota_hrg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+}

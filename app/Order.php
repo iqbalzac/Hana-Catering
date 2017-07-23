dfbeef5da0cc3f8ea77b93fde768547b445c28d3
Cendekia Pramana Putra <me@cendekiapp.com>
2017-07-23 17:19:11 +0700

    activate pesanan

--- app/Order.php
+++ app/Order.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Detail;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+	protected $primaryKey = 'id_psn';
+	
+    public function detailPesanan()
+    {
+    	return $this->hasMany(Detail::class, 'id_psn', 'id_psn');
+    }
 }

483b04deed27bfa9d121974750845b8bfa0678e0
Cendekia Pramana Putra <me@cendekiapp.com>
2017-07-25 09:57:42 +0700

    fix asset path and implement to auth page

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,10 +1,10 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
-<script src="js/jquery.min.js"></script>
-<script src="js/tether.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/animsition.min.js"></script>
-<script src="js/bootstrap-slider.min.js"></script>
-<script src="js/jquery.isotope.min.js"></script>
-<script src="js/headroom.js"></script>
-<script src="js/foodpicky.min.js"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/tether.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/animsition.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap-slider.min.js') }}"></script>
+<script src="{{ asset('js/jquery.isotope.min.js') }}"></script>
+<script src="{{ asset('js/headroom.js') }}"></script>
+<script src="{{ asset('js/foodpicky.min.js') }}"></script>
